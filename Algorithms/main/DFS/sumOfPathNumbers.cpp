d8256ae0d9e8612c28b6d7abcce9eb8198aa8e54
achotal1 <72218653+achotal1@users.noreply.github.com>
2022-09-05 20:13:26 -0400

    Create sumOfPathNumbers.cpp
    sum of every branch dfs

---
+++ Algorithms/main/DFS/sumOfPathNumbers.cpp
@@ -0,0 +1,62 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+class TreeNode {
+ public:
+  int val = 0;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int x) {
+    val = x;
+    left = right = nullptr;
+  }
+};
+
+class SumOfPathNumbers {
+ public:
+  static int findSumOfPathNumbers(TreeNode *root) {
+    int totalPathsSum = 0;
+    vector<int> digits;
+    findPathRecursive(root, digits, totalPathsSum);
+    return totalPathsSum;
+  }
+ private:
+  static void findPathRecursive(TreeNode *root, vector<int> &digits, int &sum){
+    if(root == nullptr){
+      return;
+    }
+    digits.push_back(root->val);
+    
+    if(root->left == nullptr && root->right == nullptr){
+      sum += calculateSum(digits);
+    }
+    else{
+      findPathRecursive(root->left, digits, sum);
+      findPathRecursive(root->right, digits, sum);
+    }
+    digits.pop_back();
+
+  }
+  static int calculateSum(vector<int> &digits){
+    int sum = 0;
+    int i = 0;
+    for(auto it = digits.rbegin();it != digits.rend(); it++){
+      sum += pow(10,i) * (*it);
+      i++;
+    }
+    return sum;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(0);
+  root->right = new TreeNode(1);
+  root->left->left = new TreeNode(1);
+  root->right->left = new TreeNode(6);
+  root->right->right = new TreeNode(5);
+  cout << "Total Sum of Path Numbers: " << SumOfPathNumbers::findSumOfPathNumbers(root) << endl;
+}
