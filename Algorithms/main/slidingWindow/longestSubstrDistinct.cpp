23f0019a48797299b2ff1a88bbc3ab598841aa12
achotal1 <achotal1@binghamton.edu>
2022-09-15 15:45:30 -0400

    longest substring with distinct characters problem O(n^2) time

---
+++ Algorithms/main/slidingWindow/longestSubstrDistinct.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class NoRepeatSubstring {
+ public:
+  static int findLength(const string& str) {
+    int maxLength = 0;
+    unordered_map<char, int> charMap;
+    int windowStart = 0;
+    for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+      charMap[str[windowEnd]]++;
+      while(charMap[str[windowEnd]] > 1){
+        charMap[str[windowStart]]--;
+        if(charMap[str[windowStart]] == 0){
+          charMap.erase(str[windowStart]);
+        }
+        windowStart++;
+      }
+      maxLength = max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+  }
+};
