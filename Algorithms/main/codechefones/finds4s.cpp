29e0c9f90dc57e0a183e82012b010155d5c66360
achotal1 <achotal1@binghamton.edu>
2022-10-31 15:36:56 -0400

    rldl

--- Algorithms/main/codechefones/finds4s.cpp
+++ Algorithms/main/codechefones/finds4s.cpp
@@ -21,3 +21,36 @@ int main() {
 	}
 	return 0;
 }
+-----------------------------------------------------------------------------------
+
+// RLGL fizzbuzz
+#include <iostream>
+#include <vector>
+using namespace std;
+int findShotPeople(int num, int height){
+    vector<int> v(num);
+    int c = 0;
+    for(int i = 0; i<num; i++){
+        cin>>v[i];
+    }
+    for(auto a: v){
+        if(a > height){
+            c++;
+        }
+    }
+    return c;
+}
+int main() {
+	// your code goes here
+	int t(0);
+	cin>>t;
+	for(int i = 0; i<t; i++){
+	    int num = 0;
+	    cin>>num;
+	    int height = 0;
+	    cin>>height;
+	    cout<<findShotPeople(num,height)<<'\n';
+	}
+	return 0;
+}
+
