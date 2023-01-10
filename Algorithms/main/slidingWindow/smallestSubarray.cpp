676f5d38eb20657251031f7519f2f44851d8a50b
achotal1 <achotal1@binghamton.edu>
2023-01-10 09:34:26 -0500

    simpler smaller subarray

--- Algorithms/main/slidingWindow/smallestSubarray.cpp
+++ Algorithms/main/slidingWindow/smallestSubarray.cpp
@@ -7,37 +7,18 @@ using namespace std;
 class MinSizeSubArraySum {
  public:
   static int findMinSubArray(int S, const vector<int>& arr) {
-    int windowStart(0);
-    int mini = numeric_limits<int>::max();
+    int mini = numeric_limits<int> :: max();
     int sum(0);
+    int windowStart = 0;
     for(int windowEnd = 0; windowEnd < arr.size(); windowEnd++){
       sum += arr[windowEnd];
       while(sum >= S){
-        mini = min(mini, windowEnd - windowStart + 1);
         sum -= arr[windowStart];
+        mini = min(mini, windowEnd - windowStart + 1);
         windowStart++;
       }
     }
-    return mini;
+    return mini == numeric_limits<int>::max() ? 0 : mini;
   }
-  static int MinSubArrayLen(int target, vector<int>& nums){
-    int sum = 0; int windowStart(0);
-    int mini = numeric_limits<int>::max();
-    int c = 0;
-    for(int windowEnd = 0; windowEnd < nums.size(); windowEnd++){
-        sum += nums[windowEnd];  
-        while(sum >= target && windowStart <= windowEnd){
-            if(sum == target){
-                mini = min(mini, windowEnd - windowStart + 1);
-                c = 1;
-            }
-            sum -= nums[windowStart];
-            windowStart++;
-        }
-    }
-    if(c==0){
-       return 0;
-    }
-    return mini;
-    }
 };
+
