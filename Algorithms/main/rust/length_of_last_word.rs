8e2d68a74447b91d0a80e693c6a60ee1379829c3
achotal1 <achotal1@binghamton.edu>
2023-03-24 17:29:52 -0400

    length of last word

---
+++ Algorithms/main/rust/length_of_last_word.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let s_iter:Vec<char> = s.chars().collect();
+        let mut save: i32 = 0;
+        for (i,v) in s_iter.iter().enumerate(){
+            if i != s_iter.len() - 1{
+                if *v == ' ' && s_iter[i+1] != ' '{
+                   save = (i+1) as i32;
+                }
+            }
+            
+        }
+        let mut char_count = 0;
+        let len:i32 = s_iter.len() as i32;
+        for i in save..len{
+            if s_iter[i as usize] == ' '{
+                break;
+            }
+            char_count += 1;
+        }
+        char_count
+
+    }
+}
