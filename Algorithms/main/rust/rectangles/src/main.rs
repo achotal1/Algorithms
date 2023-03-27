1ee98b9f76650a5c825fa502333fbcb7e3f74893
achotal1 <achotal1@binghamton.edu>
2023-03-26 21:40:27 -0400

    practicing vim

--- Algorithms/main/rust/rectangles/src/main.rs
+++ Algorithms/main/rust/rectangles/src/main.rs
@@ -1,33 +1,44 @@
+
+use std::io;
+
+#[allow(dead_code)]
 struct Rectangle{
-	length: f32,
-	breadth: f32
+	width: f32,
+	height: f32
 }
 
 impl Rectangle{
-	fn calcarea(&self)->f32{
-		self.length*self.breadth
+    fn square(w: f32) -> Rectangle{
+		Rectangle { width: w, height: w }
 	}
-	fn can_hold(&self,rec2: &Rectangle)->bool{
-		if self.length>rec2.length && self.breadth> rec2.breadth{
-			return true;
-		}
-		false
+	fn rectangle(w: f32, h: f32)-> Rectangle{
+		Rectangle { width: w, height: h }
 	}
-}
-fn main() {
-  	let rec1 = Rectangle{
-  				length : 12.5,
-  				breadth : 10.2,
-  	    };
-  	let rec2 = Rectangle{
-  				length : 10.5,
-  				breadth : 6.2,
-  	    };
-  	let rec3 = Rectangle{
-  				length : 9.5,
-  				breadth : 10.2,
-  	    };
-  	println!("{}",rec1.calcarea());
-  	println!("{}", rec1.can_hold(&rec3));
+	fn calc_area(&self) -> f32{
+		self.width*self.height
+	}
+
+	fn fits(&self, r2: &Rectangle) -> bool{
+		self.width >= r2.width && self.height >= r2.height	
+	}
 }
 
+fn main(){
+	println!("Enter how many rectangles you want to make: ");
+	let mut s = String::new();
+	io::stdin()
+		.read_line(&mut s).expect("Expected an integer!");
+	let s1 = s.trim().parse::<u32>().expect("yes");
+	let mut rectangles: Vec<Rectangle> = Vec::new();
+	// println!("{}", s1);
+
+	for _ in 0..s1{
+		rectangles.push(Rectangle::rectangle(5f32, 8f32));
+	}
+
+	println!("{}",rectangles[2].calc_area());
+
+	println!("{}",rectangles[2].fits(&rectangles[4]));
+
+
+}
