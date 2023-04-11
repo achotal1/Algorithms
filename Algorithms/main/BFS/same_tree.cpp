4eaa1e0c2a4ccaf89ca20c4441b6c91397a1d398
achotal1 <achotal1@binghamton.edu>
2023-04-10 21:14:11 -0400

    same tree or not

---
+++ Algorithms/main/BFS/same_tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode *> first;
+        queue<TreeNode *> second;
+        if(p == nullptr && q == nullptr){
+            return true;
+        }else if(p == nullptr || q == nullptr){
+            return false;
+        }
+        first.push(p);
+        second.push(q);
+        while(!first.empty() && !second.empty()){
+            int size = first.size();
+            
+            for(int i = 0; i<size; i++){
+                TreeNode *x = first.front();
+                TreeNode *y = second.front();
+                first.pop();
+                second.pop();
+                if(x->val != y->val){
+                    return false;
+                }
+                if(x->left != nullptr && y->left != nullptr){
+                    first.push(x->left);
+                    second.push(y->left);
+                }else if(x->left != y->left){
+                    return false;
+                }
+                if(x->right != nullptr && y->right != nullptr){
+                    first.push(x->right);
+                    second.push(y->right);
+                }else if(x->right != y->right){
+                    return false;
+                }
+            }
+        }
+        if(first.empty() != second.empty()){
+            return false;
+        }
+        return true;
+    }
+};
