f22b1728af764570fa00473d50b142c4f23da62e
achotal1 <achotal1@binghamton.edu>
2023-04-15 22:11:09 -0400

    duplicates in LL

--- Algorithms/main/revlinkedlist/removeDupsLL.cpp
+++ Algorithms/main/revlinkedlist/removeDupsLL.cpp
@@ -48,3 +48,54 @@ string LinkedList::removeDuplicates() {
   
   return elements();
 }
+
+/**
+ * Sorted LinkedList remove dups
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *curr = head->next;
+        ListNode *prev = head;
+        if(prev == nullptr){
+            return nullptr;
+        }
+        while(curr != nullptr){
+            if(curr->val == prev->val){
+                prev->next = curr->next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = curr->next;
+            
+        }
+        return head;
+    }
+    ListNode* deleteDups2(ListNode *head){
+      if(head == nullptr){
+            return head;
+        }
+        ListNode *curr = head;
+        while(curr  && curr->next ){
+            if(curr->val == curr->next->val){
+                ListNode *temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            }else{
+                curr = curr->next;
+            }
+            
+        }
+
+        return head;
+    }
+};
