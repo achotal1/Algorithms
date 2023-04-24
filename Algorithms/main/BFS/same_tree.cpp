fe6d34c1d6a8b816e29e244462a8b75a184e514c
achotal1 <achotal1@binghamton.edu>
2023-04-24 12:17:06 -0400

    added dfs solution for same_tree problem

--- Algorithms/main/BFS/same_tree.cpp
+++ Algorithms/main/BFS/same_tree.cpp
@@ -51,4 +51,18 @@ public:
         }
         return true;
     }
+    // via dfs
+    bool isSameTreeDFS(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr){
+            return true;
+        }else if(p == nullptr){
+            return false;
+        }else if(q == nullptr){
+            return false;
+        }
+        if(p->val != q->val){
+            return false;
+        }
+        return isSameTreeDFS(p->left, q->left) && isSameTreeDFS(p->right, q->right);
+    }
 };
