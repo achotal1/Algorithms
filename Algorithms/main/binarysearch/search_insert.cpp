2afecfdc89eb0233fb6515af1e38d7b983ec526a
achotal1 <achotal1@binghamton.edu>
2024-02-21 11:41:08 -0500

    better binary search

--- Algorithms/main/binarysearch/search_insert.cpp
+++ Algorithms/main/binarysearch/search_insert.cpp
@@ -19,3 +19,23 @@ public:
         }
     }
 };
+
+Better version:
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        int mid = 0;
+        while(left < right){
+            mid = left + (right - left)/2;
+            if(nums[mid] >= target){
+                right = mid;
+            }
+            else
+                left = mid + 1;
+           
+        }
+        return left;
+    }
+};
